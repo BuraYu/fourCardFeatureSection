79e527a81eabf882474c7229386ee9f8653d3c11
BuraYu <burak.yueksel@hotmail.com>
2024-03-05 22:58:12 +0100

    rem flex, add grid, fixed media querry

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Card.css";
 
-const card = ({ title, description, logo, color }) => {
+const card = ({ title, description, logo, color, div }) => {
   return (
-    <div className="card" style={{ borderTop: `4px solid ${color}` }}>
+    <div className={`card ${div}`} style={{ borderTop: `4px solid ${color}` }}>
       <span className="card-title">{title}</span>
       <p className="card-description">{description}</p>
       <img src={logo} alt="" className="card-img" />
